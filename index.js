8f5cf37e65ca7996978570dbf53cd44ece16d42e
Brian Bozigian <bbozigian@outlook.com>
2021-09-02 04:36:22 +0000

    new

---
+++ index.js
@@ -0,0 +1,8 @@
+// Write a function that takes an integer minutes and converts it to seconds.
+// convert(5) -> 120
+// convert(3) -> 120
+
+function convert(minutes) {
+  const min = minutes * 60
+  return min
+}
