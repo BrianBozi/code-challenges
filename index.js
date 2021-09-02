12c15f494afbb15de52dc5b97ba27ec4f42dfab5
Brian Bozigian <bbozigian@outlook.com>
2021-09-02 04:37:34 +0000

    test

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ function convert(minutes) {
   const min = minutes * 60
   return min
 }
+
+// Return the Sum of Two Numbers
+// addition(3, 2) ➞ 5
+// addition(-3, -6) ➞ -9
+// addition(7, 3) ➞ 10
+
+function addition (x , y ) {
+  return x + y
+}
+
+// Convert Hours into Seconds
+// howManySeconds(2) ➞ 7200
+// howManySeconds(10) ➞ 36000
+// howManySeconds(24) ➞ 86400
+function howManySeconds(hours){
+  var sec = 60 * 60;
+  var total = hours * sec;
+  return total
+}
+
+// ;lasdas
